a6b4ec6833097692468b6556ec61cc63d17c6e14
Luis Diaz <diazluis1048@gmail.com>
2017-12-26 18:40:33 -0500

    began work on js file by defining user interface variables

---
+++ ToDoList/app.js
@@ -0,0 +1,6 @@
+//Define User Interface Variables
+const form = document.querySelector(".task-form");
+const taskList = document.querySelector(".collection");
+const clearButton = document.querySelector("clear-tasks");
+const filter = document.querySelector(".filter");
+const taskInput = document.querySelector(".tasks");
