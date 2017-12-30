e3956f61f5eae0623de0a63c5b5e6a95d338c423
Luis Diaz <diazluis1048@gmail.com>
2017-12-30 13:10:00 -0500

    created function necessary to persist data to local storage.  updated readme as well

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Houses multiple vanilla JavaScript projects.
 
-To Do List: A To Do List that dynamically inserts tasks into the DOM
+To Do List: A To Do List that dynamically inserts tasks into the DOM. Users are able to delete tasks entered as well as search for them. Application persists to local storage. 
 
 Purpose:
 
@@ -10,6 +10,6 @@ Concepts learned:
 
 Event Bubbling: When an event is  triggered, the event bubbles up from the originating element all the way up to the top of the DOM tree.
 
-Event Delegation: A simple technique by which we add a single event handler to a parent element in order to avoid having to add event handlers to multiple child elements.  Using event delegation, it is possible to add an event handler to an element, wait for the event to bubble up from a child element and easily determine from which element the event originated from. 
+Event Delegation: A simple technique by which we add a single event handler to a parent element in order to avoid having to add event handlers to multiple child elements.  Using event delegation, it is possible to add an event handler to an element, wait for the event to bubble up from a child element and easily determine from which element the event originated from.
 
 For event listeners to be bounded on elements, they MUST BE PRESENT AT PAGE LOAD. This becomes an issue when dynamically inserting elements to DOM.
--- ToDoList/app.js
+++ ToDoList/app.js
@@ -5,11 +5,13 @@ const clearButton = document.querySelector(".clear-tasks");
 const filter = document.querySelector(".filter");
 const taskInput = document.querySelector(".task");
 
-//Load all event listeners
+
 loadEventListeners();//Listing all event listeners outside of function would give them global scope
 
 //Load all event loadEventListeners
 function loadEventListeners() {
+    //DOM Load event
+    document.addEventListener("DOMContentLoaded", getTasks);//The DOMContentLoaded event is fired when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
     //Attaches event listener to form.
     form.addEventListener("submit", addTask);//The submit event is fired when a form is submitted.
     //attaches event listener to taskList
@@ -18,6 +20,35 @@ function loadEventListeners() {
     clearButton.addEventListener("click", clearTasks);
     //filter tasks event
     filter.addEventListener("keyup", filterTasks);
+}
+
+//Get Tasks from LS
+function getTasks () {
+    let tasks;
+    if(localStorage.getItem("tasks" === null )){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    tasks.forEach(function(task){
+        //Create li element
+        const li = document.createElement("li");
+
+        //Adds a class to li element
+        li.className = "collection-item";
+        //Create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        //Create new link element
+        const link = document.createElement("a");
+        link.className = "delete-item secondary-content";
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //append the link to li
+        li.appendChild(link);
+
+        //append li to ul
+        taskList.appendChild(li);
+        });
 }
 
 //add tasks
@@ -39,8 +70,13 @@ function addTask(e) {//If a task hasn't been entered, asks user to input one.
     link.innerHTML = '<i class="fa fa-remove"></i>';
     //append the link to li
     li.appendChild(link);
+
     //append li to ul
     taskList.appendChild(li);
+
+    //Store in Local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     //clear input once user inputs task
     taskInput.value = "";
 
@@ -48,13 +84,50 @@ function addTask(e) {//If a task hasn't been entered, asks user to input one.
     e.preventDefault();//prevents the default behavior that each element has
 }
 
+// Store Task
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem("tasks" === null )){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
 //remove tasks
 function removeTask(e) {
     if (e.target.parentElement.classList.contains("delete-item")){//displays the child element that triggered the event ONLY IF its parent element contains delete-item as one of its classes
         if(confirm("Are you sure you wish to delete this element?")){
             e.target.parentElement.parentElement.remove();//the first parentElement brings you to the a tag. The next one brings you to the li
+
+            //Remove from LS
+            removeTaskfromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+//Remove from local localStorage
+function removeTaskfromLocalStorage(taskItem) {
+    let tasks;
+    if(localStorage.getItem("tasks" === null )){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //Clears taskList
@@ -62,7 +135,16 @@ function clearTasks() {
     while (taskList.firstChild) {//
         taskList.removeChild(taskList.firstChild);
     }
+
+    //Clear from Local Storage
+    clearTasksFromLocalStorage();
 }
+
+//Clear Tasks from Local storage
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
+}
+
 //Allows user to search for tasks
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
