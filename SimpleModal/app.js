a789ba632a5af08e3f1ff7b48f416e2e983b1b58
Luis Diaz <diazluis1048@gmail.com>
2018-01-15 17:49:50 -0500

    defined UI elements in JS file, and added event listeners/functions to handle events

---
+++ SimpleModal/app.js
@@ -0,0 +1,28 @@
+//Define UI elements
+const modal = document.querySelector('.simpleModal');
+const modalBtn = document.querySelector('.modalBtn');
+const closeBtn = document.querySelector('.closeBtn');
+
+// Listen for open  click
+modalBtn.addEventListener('click', openModal);
+// Listen for open  click
+closeBtn.addEventListener('click', closeModal);
+//Listen for click outside of modal
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+//function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+//Function to close modal if user clicks outside modal container
+function outsideClick(e) {
+    if(e.target === modal) {
+        modal.style.display = 'none';
+    }
+}
