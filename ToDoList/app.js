e0d76903be063ce23f11a2b285bc7eddc5d3c62f
Luis Diaz <diazluis1048@gmail.com>
2018-03-21 19:37:21 -0400

    fixed local storage bug

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,163 +1,160 @@
-//Define User Interface Variables
-const form = document.querySelector(".task-form");
-const taskList = document.querySelector(".collection");
-const clearButton = document.querySelector(".clear-tasks");
-const filter = document.querySelector(".filter");
-const taskInput = document.querySelector(".task");
+// Define UI Vars
+const form = document.querySelector('.task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('.filter');
+const taskInput = document.querySelector('.task');
 
-
+// Load all event listeners
 loadEventListeners();//Listing all event listeners outside of function would give them global scope
 
-//Load all event loadEventListeners
+// Load all event listeners
 function loadEventListeners() {
-    //DOM Load event
-    document.addEventListener("DOMContentLoaded", getTasks);//The DOMContentLoaded event is fired when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-    //Attaches event listener to form.
-    form.addEventListener("submit", addTask);//The submit event is fired when a form is submitted.
-    //attaches event listener to taskList
-    taskList.addEventListener("click", removeTask);
-    //attaches event listener to clear tasks button
-    clearButton.addEventListener("click", clearTasks);
-    //filter tasks event
-    filter.addEventListener("keyup", filterTasks);
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks); //The DOMContentLoaded event is fired when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+  // Add task event
+  form.addEventListener('submit', addTask);//The submit event is fired when a form is submitted.
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
-//Get Tasks from LS
-function getTasks () {
-    let tasks;
-    if(localStorage.getItem("tasks" === null )){
-        tasks = [];
-    }
-    else{
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-    tasks.forEach(function(task){
-        //Create li element
-        const li = document.createElement("li");
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
 
-        //Adds a class to li element
-        li.className = "collection-item";
-        //Create text node and append to li
-        li.appendChild(document.createTextNode(task));
-        //Create new link element
-        const link = document.createElement("a");
-        link.className = "delete-item secondary-content";
-        link.innerHTML = '<i class="fa fa-remove"></i>';
-        //append the link to li
-        li.appendChild(link);
-
-        //append li to ul
-        taskList.appendChild(li);
-        });
-}
-
-//add tasks
-function addTask(e) {//If a task hasn't been entered, asks user to input one.
-    if(taskInput.value === "") {
-        alert("Please add a task.");
-    }
-
-    //Create li element
-    const li = document.createElement("li");
-
-    //Adds a class to li element
-    li.className = "collection-item";
-    //Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    //Create new link element
-    const link = document.createElement("a");
-    link.className = "delete-item secondary-content";
+  tasks.forEach(function(task){
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
     link.innerHTML = '<i class="fa fa-remove"></i>';
-    //append the link to li
+    // Append the link to li
     li.appendChild(link);
 
-    //append li to ul
+    // Append li to ul
     taskList.appendChild(li);
+  });
+}
 
-    //Store in Local storage
-    storeTaskInLocalStorage(taskInput.value);
+// Add Task
+function addTask(e) {//If a task hasn't been entered, asks user to input one.
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
 
-    //clear input once user inputs task
-    taskInput.value = "";
+  // Create li element
+  const li = document.createElement('li');
 
+  // Adds a class to li element
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
 
-    e.preventDefault();//prevents the default behavior that each element has
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();//prevents the default behavior that each element has
 }
 
 // Store Task
 function storeTaskInLocalStorage(task) {
-    let tasks;
-    if(localStorage.getItem("tasks" === null )){
-        tasks = [];
-    }
-    else{
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-    tasks.push(task);
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-
-//remove tasks
+// Remove Tasks
 function removeTask(e) {
-    if (e.target.parentElement.classList.contains("delete-item")){//displays the child element that triggered the event ONLY IF its parent element contains delete-item as one of its classes
-        if(confirm("Are you sure you wish to delete this element?")){
-            e.target.parentElement.parentElement.remove();//the first parentElement brings you to the a tag. The next one brings you to the li
+  if(e.target.parentElement.classList.contains('delete-item')) { //displays the child element that triggered the event ONLY IF its parent element contains delete-item as one of its classes
+    if(confirm('Are you sure you wish to delete this element?')) {
+      e.target.parentElement.parentElement.remove();//the first parentElement brings you to the a tag. The next one brings you to the li
 
-            //Remove from LS
-            removeTaskfromLocalStorage(e.target.parentElement.parentElement);
-        }
+      // Remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
+  }
 }
 
-//Remove from local localStorage
-function removeTaskfromLocalStorage(taskItem) {
-    let tasks;
-    if(localStorage.getItem("tasks" === null )){
-        tasks = [];
-    }
-    else{
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
+// Remove from localStorage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
 
-    tasks.forEach(function(task, index){
-        if(taskItem.textContent === task){
-            tasks.splice(index, 1);
-        }
-    });
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
+    }
+  });
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-//Clears taskList
+// Clear Tasks
 function clearTasks() {
-    while (taskList.firstChild) {//
-        taskList.removeChild(taskList.firstChild);
-    }
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 
-    //Clear from Local Storage
-    clearTasksFromLocalStorage();
+  // Clear from LS
+  clearTasksFromLocalStorage();
 }
 
-//Clear Tasks from Local storage
-function clearTasksFromLocalStorage(){
-    localStorage.clear();
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
-//Allows user to search for tasks
+// Allows user to search for tasks
 function filterTasks(e) {
-    const text = e.target.value.toLowerCase();
+  const text = e.target.value.toLowerCase();
 
-    document.querySelectorAll(".collection-item").forEach(
-        function(task){
-            const item = task.firstChild.textContent;//textContent property sets or returns the textual content of the specified node, and all its descendants.
-            if(item.toLowerCase().indexOf(text) != -1){ // -1 means no match
-                task.style.display = "block";
-            }
-            else{
-                task.style.display = "none";
-            }
-        }
-    );
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;//textContent property sets or returns the textual content of the specified node, and all its descendants.
+    if(item.toLowerCase().indexOf(text) != -1){ // -1 means no match
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
