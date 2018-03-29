40c51eecbd08706614b275a299f579b16f2d69c9
Luis Diaz <diazluis1048@gmail.com>
2018-03-29 14:14:10 -0400

    created xhr object methods that display text from .txt file to browser

---
+++ AjaxSandbox/app.js
@@ -0,0 +1,52 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
+
+  // OPEN specifies the type of request we want to make and the file name we want to make it to
+  xhr.open('GET', 'data.txt', true);
+
+  // console.log('READYSTATE', xhr.readyState);
+
+  // Optional - Used for spinners/loaders
+  //used to display while its fetching the data/loading
+  xhr.onprogress = function(){
+    console.log('READYSTATE', xhr.readyState);
+  }
+
+  xhr.onload = function(){
+    console.log('READYSTATE', xhr.readyState);
+    if(this.status === 200) {
+      // console.log(this.responseText);
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+    }
+  }
+
+  // xhr.onreadystatechange = function() {
+  //   console.log('READYSTATE', xhr.readyState);
+  //   if(this.status === 200 && this.readyState === 4){
+  //     console.log(this.responseText);
+  //   }
+  // }
+
+  xhr.onerror = function() {
+    console.log('Request error...');
+  }
+
+  xhr.send();
+
+
+    // readyState Values
+    // 0: request not initialized
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+
+
+  // HTTP Statuses
+  // 200: "OK"
+  // 403: "Forbidden"
+  // 404: "Not Found"
+}
