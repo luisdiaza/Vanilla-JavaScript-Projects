0c3d493c43230dc7a148eda5b1e068d9c4cc77ad
Luis Diaz <diazluis1048@gmail.com>
2018-03-30 21:21:25 -0400

    reworked project to retrieve data from json files and output to browser

---
+++ AjaxSandbox/app2.js
@@ -0,0 +1,67 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();//creates an xhr request and instantiates it. They interact with servers
+                                  //and retrieve data from a URL without having to do a full page refresh
+                                 //Can be used to retrieve any type of data
+
+  xhr.open('GET', 'customer.json', true);//retrieves data from customer.json file.
+                                         //true is for asynchronous
+
+  xhr.onload = function(){
+    if(this.status === 200) {//if website loads properly
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);//parses as an object
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
+
+
+// Load Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function(){
+    if(this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer){
+        output += `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
